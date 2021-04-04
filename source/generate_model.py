543c264f2c915ecf8c27c38c23aad3ed89528ce2
guillaume <Guillaume.Teboul-Tornezy@ulb.be>
2021-04-04 15:13:01 +0200

    begining of the writing

--- source/generate_model.py
+++ source/generate_model.py
@@ -1,9 +1,17 @@
 import sys
+import os
 
 class GenerateModel:
 
-	def __init__(self , penalty):
+
+	def __init__(self , penalty , fileName):
+
 		self.penalty = penalty
+		self.fileToParse = fileName;
+		self.instanceNumber = fileName[:-4][-1]
+
+		self.outputFileName = ""
+
 		self.I = 0
 		self.J = 0
 		self.K = 0
@@ -13,21 +21,19 @@ class GenerateModel:
 		self.cj = []
 
 		self.tij = []
-		self.Sk = []
+		self.S = []
+		self.pk = []
 
-	def printData(self):
-		print("I= " , self.I , "J= ", self.J,"K= " , self.K)
 
-		print("fj " , self.fj)
-		print("di " , self.di)
-		print("cj " , self.cj)
+	def startGenerating(self):
+		self.parseFile();
+		self.createPenalty();
+		self.createFile();
+		self.writeFile();
 
-		for i in range(len(self.tij)):
-			print("tij ", self.tij[i])
-		for k in range(len(self.Sk)):
-			print("Sk " , self.Sk[k])
+	def parseFile(self):
 
-	def parseFile(self , fileName):
+		fileName = self.fileToParse;
 		with open(fileName) as fd:
 			line = fd.readline().split(" ")
 			self.I = int(line[0])
@@ -42,8 +48,66 @@ class GenerateModel:
 				self.tij.append(fd.readline().strip("\n").split(" "))
 
 			for k in range(self.K):
-				self.Sk.append(fd.readline().strip("\n").split(" "))
+				self.S.append(fd.readline().strip("\n").split(" "))
 
+	def createPenalty(self):
+		for k in range(len(self.S)):
+			self.pk.append(self.S[k][-1])
+			self.S[k] = self.S[k][:-1]
+
+	def createFile(self):
+		self.outputFileName = "model" + "_" + str(self.penalty) + "_" +str(self.I) +"_" + str(self.J) + "_" +self.instanceNumber + ".lp"
+		if not os.path.exists(self.outputFileName):
+			os.mknod(self.outputFileName)
+
+	def writeObjFunction(self):
+		objFunctionString = "Minimisze\n\tobj: "
+		for j in range(self.J):
+			objFunctionString += "x_"+str(j+1)+ " " + str(self.fj[j])+ " + "
+			for i in range(self.I):
+				objFunctionString += str(self.tij[i][j]) + " " + "y_" + str(i+1) + "_" + str(j+1) + " + "
+
+		for k in range(len(self.pk)):
+			objFunctionString += "z_" + str(k+1) + " " + str(self.pk[k]) + " + "
+
+		objFunctionString = objFunctionString[:-3]
+		return objFunctionString + "\n"
+
+	def writeBinary(self):
+		binaryString = "Binary\n"
+		for j in range(self.J):
+			binaryString += "\tx_" +str(j+1)+"\n"
+
+		for j in range(self.J):
+			for i in range(self.I):
+				binaryString += "\ty_" + str(i+1) + "_" + str(j+1)+"\n"
+
+		for k in range(len(self.S)):
+			binaryString += "\tz_" + str(k+1) + "\n"
+
+		return binaryString
+
+	def writeFile(self):
+		file = open(self.outputFileName, "w")
+		file.write(self.writeObjFunction())
+		file.write(self.writeBinary())
+		file.write("END")
+		file.close()
+
+
+	def printData(self):
+		print("I= " , self.I , "J= ", self.J,"K= " , self.K)
+
+		print("fj " , self.fj)
+		print("di " , self.di)
+		print("cj " , self.cj)
+
+		for i in range(len(self.tij)):
+			print("tij ", self.tij[i])
+		for k in range(len(self.S)):
+			print("S " , k , " " , self.S[k])
+
+		print("pk " , self.pk)
 
 
 if __name__ == "__main__":
@@ -51,6 +115,5 @@ if __name__ == "__main__":
 		print("Erreur, il n'y a pas tout les arguments")
 		sys.exit(1) 
 	else:
-		model = GenerateModel(int(sys.argv[2]))
-		model.parseFile(sys.argv[1])
-		model.printData()
+		model = GenerateModel(int(sys.argv[2]) , sys.argv[1])
+		model.startGenerating()
